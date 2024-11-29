2c60c042d71f187989cd95ff830c02a63c6c262e
mng <1379384689@qq.com>
2024-11-29 15:02:59 +0800

    Site updated: 2024-11-29 15:02:59

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732863381696";
+const VERSION = "1732863773883";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
